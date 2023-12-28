802d9227cccd729c4df8594906f4b147731b40e5
phoenix <naman.j@consultadd.com>
2023-12-28 10:23:28 -0600

    Created view for get endpoint.

--- student/views.py
+++ student/views.py
@@ -2,6 +2,21 @@ from .models import Student
 from django.http import JsonResponse
 from django.shortcuts import HttpResponse
 
-
 # Create your views here.
 STUDENT = Student()
+
+
+def get_data(request):
+    if request.method == 'GET':
+        try:
+            pk = request.GET.get('pk')
+            details = Student.objects.get(pk=pk)
+            data = {'id': details.id, 'name': details.name, 'age': details.age, 'address': details.address}
+            # print(data)
+
+            return JsonResponse(data)
+        except Exception as e:
+            return HttpResponse(f"<h2 style='font-family:Helvetica'>Error:{e}</br></br>No Data Found.</h2>")
+
+    else:
+        return HttpResponse("<h2 style='font-family:Helvetica'>No data found</h2>")
