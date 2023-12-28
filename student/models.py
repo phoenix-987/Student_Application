6c049762ba0892ec3ff3a5366180403ebe576551
phoenix <naman.j@consultadd.com>
2023-12-28 10:08:11 -0600

    Initial Commit.

---
+++ student/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Student(models.Model):
+    id = models.CharField(primary_key=True, max_length=5, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
+    address = models.CharField(max_length=255, blank=False, null=False)
---
+++ student/views.py
@@ -0,0 +1,7 @@
+from .models import Student
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse
+
+
+# Create your views here.
+STUDENT = Student()
